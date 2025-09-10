c25d20cf95b96c3e3b50e167edd59173b5651690
Raphael <raphaelgarnier1997@gmail.com>
2025-09-10 19:58:51 +0200

    Creating the Footer section & Styling it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import TestimonialCarrousel from "@/components/testimonial-carrousel";
 
 export default function HomePage() {
   return (
-    <main>
+    <main className="w-full overflow-x-hidden">
       <section className="w-full relative h-screen">
         <Image
           src="https://images.unsplash.com/photo-1586023492125-27b2c045efd7?q=80&w=3058&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -123,7 +123,7 @@ export default function HomePage() {
         </div>
       </section>
 
-      <section className="flex flex-col items-center mt-[170px] pt-6 px-5 text-black bg-[#F7F7F7]">
+      <section className="flex flex-col items-center mt-[170px] pt-6 px-5 text-black ">
         <h2 className="text-orange-400 text-[18px] mb-5 tracking-widest">TESTIMONIAL</h2>
         <h1 className="text-[42px] font-bold mb-[60px]">Our Client Reviews</h1>
         <div className="flex items-center">
@@ -131,7 +131,48 @@ export default function HomePage() {
         </div>
       </section>
 
-      <footer></footer>
+      <footer className="mt-[230px]  px-[186px] text-black w-full bg-[#F7F7F7]">
+        <div className="pt-[118px] grid grid-cols-12 gap-x-[110px]">
+          <div className="col-span-5">
+            <h2 className=" font-extrabold text-2xl tracking-widest mb-7">PANTO</h2>
+            <p className="text-base/8 max-w-[300px]">
+              The advantage of hiring a workspace with us is that givees you comfortable
+              service and all-around facilities.
+            </p>
+          </div>
+          <div className="col-span-2">
+            <h2 className="text-orange-400 tracking-widest mb-7">Services</h2>
+            <ul className="space-y-4 whitespace-nowrap">
+              <li>Email Marketing</li>
+              <li>Campaigns</li>
+              <li>Branding</li>
+            </ul>
+          </div>
+          <div className="col-span-2">
+            <h2 className="text-orange-400 tracking-widest mb-7">Furniture</h2>
+            <ul className="space-y-4 whitespace-nowrap">
+              <li>Beds</li>
+              <li>Chair</li>
+              <li>All</li>
+            </ul>
+          </div>
+          <div className="col-span-2">
+            <h2 className="text-orange-400 tracking-widest mb-7">Follow us</h2>
+            <ul className="space-y-4 whitespace-nowrap">
+              <li>Facebook</li>
+              <li>Twitter</li>
+              <li>Instagram</li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex justify-between mt-[120px] pb-[58px]  ">
+          <p>Copyright &copy; 2025</p>
+          <div className="flex gap-x-8">
+            <p>Terms & Conditions</p>
+            <p>Privacy Policy</p>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
