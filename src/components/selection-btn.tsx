1d50115a7da08703745ce9a22d7f25a450f50829
Raphael <raphaelgarnier1997@gmail.com>
2025-09-11 04:00:27 +0200

    Adding a animation via framer motion for the selectionBtn component

--- src/components/selection-btn.tsx
+++ src/components/selection-btn.tsx
@@ -1,32 +1,33 @@
+"use client";
+
+import { useState } from "react";
+import { motion } from "motion/react";
 import { Button } from "./ui/button";
 
 export default function SelectionBtn() {
+  const [selected, setSelected] = useState("Chair");
+  const items = ["Chair", "Beds", "Sofa", "Lamps"];
+
   return (
-    <div className="bg-[#EEEEEE] px-4 py-4 rounded-3xl mt-[32px] w-[380px] h-[54px] flex justify-between items-center">
-      <Button
-        variant="ghost"
-        className="active:bg-white text-sm text-black rounded-3xl w-[84px] h-[45px]"
-      >
-        Chair
-      </Button>
-      <Button
-        variant="ghost"
-        className="active:bg-white text-sm text-black rounded-3xl w-[84px] h-[45px]"
-      >
-        Beds
-      </Button>
-      <Button
-        variant="ghost"
-        className="active:bg-white text-sm text-black rounded-3xl w-[84px] h-[45px]"
-      >
-        Sofa
-      </Button>
-      <Button
-        variant="ghost"
-        className="active:bg-white text-sm text-black rounded-3xl w-[84px] h-[45px]"
-      >
-        Lamps
-      </Button>
+    <div className="bg-[#EEEEEE] px-4 py-4 rounded-3xl mt-[32px] w-[380px] h-[57px] flex justify-between items-center">
+      {items.map(item => (
+        <div key={item} className="relative">
+          {selected === item && (
+            <motion.div
+              layoutId="highlight"
+              className="absolute inset-0 bg-white rounded-3xl "
+              transition={{ type: "spring", stiffness: 300, damping: 30 }}
+            />
+          )}
+          <Button
+            variant="ghost"
+            onClick={() => setSelected(item)}
+            className="text-sm text-black rounded-3xl w-[84px] h-[40px] relative z-10 hover:bg-transparent"
+          >
+            {item}
+          </Button>
+        </div>
+      ))}
     </div>
   );
 }
