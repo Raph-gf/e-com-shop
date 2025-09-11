ef89a4e35988ae640c90508cdf37451d572d7ea9
Raphael <raphaelgarnier1997@gmail.com>
2025-09-11 23:40:56 +0200

    Playing around with framer-motion

--- src/app/(public)/products/page.tsx
+++ src/app/(public)/products/page.tsx
@@ -1,38 +1,37 @@
-"use client";
-
 import CatalogueCard from "@/components/catalogue-components/catalogue-card";
 import CataloguePageHero from "@/components/catalogue-components/catalogue-hero";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { motion } from "motion/react";
+import * as motion from "motion/react-client";
 import Image from "next/image";
 
 export default function CataloguePage() {
   return (
     <main>
+      <section className="w-full relative h-screen">
+        <Image
+          src="https://images.unsplash.com/photo-1586023492125-27b2c045efd7?q=80&w=3058&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="landing page background image"
+          fill
+          className="z-0 object-cover"
+          priority
+        />
+
+        <CataloguePageHero />
+      </section>
+
       <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.1 }}
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.7, delay: 0.1 }}
       >
-        <section className="w-full relative h-screen">
-          <Image
-            src="https://images.unsplash.com/photo-1586023492125-27b2c045efd7?q=80&w=3058&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="landing page background image"
-            fill
-            className="z-0 object-cover"
-            priority
-          />
-          <CataloguePageHero />
+        <section className="mt-10 px-5 text-black">
+          <h1 className="text-[70px]  mb-5 text-center">Our Collection of Product</h1>
+          <div className="flex justify-center">
+            <Input className="w-full max-w-[950px] rounded-2xl" placeholder="Search..." />
+          </div>
         </section>
       </motion.div>
-
-      <section className="mt-10 px-5 text-black">
-        <h1 className="text-[70px]  mb-5 text-center">Our Collection of Product</h1>
-        <div className="flex justify-center">
-          <Input className="w-full max-w-[950px] rounded-2xl" placeholder="Search..." />
-        </div>
-      </section>
 
       <section className="mt-10 px-5 text-black max-w-[1440px] mx-auto">
         <div className="text-center max-w-2xl mx-auto">
@@ -58,8 +57,8 @@ export default function CataloguePage() {
             grid-cols-1 
             sm:grid-cols-2 
             lg:grid-cols-3 
-            gap-y-8
-            gap-x-5
+            gap-y-10
+            gap-x-6
             justify-items-center
           "
         >
@@ -69,7 +68,7 @@ export default function CataloguePage() {
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: index * 0.1 }}
-                whileHover={{ scale: 1.05 }}
+                whileHover={{ scale: 1.003 }}
                 whileTap={{ scale: 0.95 }}
               >
                 <CatalogueCard />
--- src/components/catalogue-components/catalogue-hero.tsx
+++ src/components/catalogue-components/catalogue-hero.tsx
@@ -1,6 +1,13 @@
+import * as motion from "motion/react-client";
+
 export default function CataloguePageHero() {
   return (
-    <div className="w-full absolute z-10 text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <motion.div
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.7, delay: 0.1 }}
+      className="w-full absolute z-10 text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+    >
       <div className="m-w-[1000px] flex flex-col justify-center text-center items-center gap-4 text-7xl">
         <span>Search For Your Favorite Interior </span>
         <span>Minimalist & Modern Furnitures</span>
@@ -9,6 +16,6 @@ export default function CataloguePageHero() {
           <span className="text-lg">and modern with ease and speed</span>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
